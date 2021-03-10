c82ac1393282b7c43956f6835f5d9e361c26f0f6
Patrick Northon <northon_patrick3@yahoo.ca>
2021-03-10 03:30:55 -0500

    Remove systemctl disable lijnes from uninstall.sh.

--- uninstall.sh
+++ uninstall.sh
@@ -2,10 +2,6 @@
 
 PREFIX=$1
 
-systemctl disable --now "netns-helper-dhcp@"
-systemctl disable --now "netns-helper-macvlan@"
-systemctl disable --now "netns-helper@"
-
 rm -f "$PREFIX/lib/systemd/system/netns-helper"/*
 rm -f "$PREFIX/bin/netns-helperctl"
 
