c0bc517e0a705e074087ab0514e6362923279c26
Patrick Northon <northon_patrick3@yahoo.ca>
2021-11-26 10:18:56 -0500

    * Update scripts' interpreters. * Remove use of env command in service files.

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 PREFIX="${PREFIX:-/usr/local}"
 
 rm -rvf "$PREFIX/lib/systemd/system/netns-helper"*
 rm -rvf "$PREFIX/lib/netns-helper"
 rm -vf "$PREFIX/bin/netns-helper"
-
-systemctl daemon-reload
